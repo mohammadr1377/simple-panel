5630d955e39d54bcf1f9211284fa098ca66295e2
mohammd Rangraz <mohammd.Rangraz@gmail.com>
2023-01-01 12:00:47 +0330

    call api get user

--- src/Page/User/Index.tsx
+++ src/Page/User/Index.tsx
@@ -1,4 +1,11 @@
+import {useEffect} from "react"
+import { GetUsers } from "./_srv"
 export default function UserPage(){
+    useEffect(()=>{
+        GetUsers()
+        .then(res=>{})
+        .catch(err=>{})
+    },[])
     return(
         <h3>کاربران</h3>
     )
